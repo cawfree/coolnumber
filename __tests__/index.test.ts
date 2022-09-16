32c4cd2d640531b46293e121218dccbb7fb7a939
cawfree <hello@cawfree.com>
2022-09-16 01:07:32 +0100

    chore: cool numbers

---
+++ __tests__/index.test.ts
@@ -0,0 +1,187 @@
+import 'jest';
+
+import BigDecimal from 'js-big-decimal';
+
+import {
+  abs,
+  BD_ONE,
+  BD_ZERO,
+  endsWithCoolNumber,
+  filterValidRange,
+  getMinDelta,
+  minMaxCoolNumber,
+  pickClosestCoolNumber,
+  toDecimalsString,
+  toCoolNumber,
+} from '../src';
+
+describe('coolnumber', () => {
+  it('jest', () => expect(true).toBeTruthy());
+
+  it('constants', () => {
+    expect(BD_ZERO.getValue()).toBe('0');
+    expect(BD_ONE.getValue()).toBe('1');
+  });
+
+  it('toDecimalsString', () => {
+
+    expect(() => toDecimalsString(BD_ZERO, -1))
+      .toThrowError('Precision must be greater than or equal to zero.');
+
+    // Default precision.
+    expect(toDecimalsString(BD_ZERO)).toBe('0.000000000000000000');
+    expect(toDecimalsString(BD_ONE)).toBe('1.000000000000000000');
+
+    // OpenSea supports up to 4 decimal places.
+    expect(toDecimalsString(BD_ONE, 4)).toBe('1.0000');
+    expect(toDecimalsString(new BigDecimal('1.1111'), 4)).toBe('1.1111');
+    expect(toDecimalsString(new BigDecimal('1.11111'), 4)).toBe('1.1111');
+    expect(toDecimalsString(new BigDecimal('1.11'), 4)).toBe('1.1100');
+
+    expect(toDecimalsString(new BigDecimal('1.11'), 0)).toBe('1');
+    expect(toDecimalsString(new BigDecimal('-1.11'), 0)).toBe('-1');
+    expect(toDecimalsString(new BigDecimal('1.11'), 1)).toBe('1.1');
+    expect(toDecimalsString(new BigDecimal('-1.11'), 1)).toBe('-1.1');
+  });
+
+  it('getMinDelta', () => {
+    expect(() => getMinDelta(-1))
+      .toThrowError('Precision must be greater than or equal to zero.');
+    expect(getMinDelta(1).getValue())
+      .toBe('0.1');
+    expect(getMinDelta().getValue())
+      .toBe('0.000000000000000001');
+    expect(getMinDelta(0).getValue())
+      .toBe('1');
+  });
+  it('endsWithCoolNumber', () => {
+    expect(endsWithCoolNumber(new BigDecimal('420'), 420))
+      .toBe(true);
+    expect(endsWithCoolNumber(new BigDecimal('420'), '420'))
+      .toBe(true);
+    expect(endsWithCoolNumber(new BigDecimal('4.20'), '420'))
+      .toBe(true);
+    expect(endsWithCoolNumber(new BigDecimal('4.69'), '420'))
+      .toBe(false);
+    expect(endsWithCoolNumber(new BigDecimal('4.69'), '69'))
+      .toBe(true);
+    expect(endsWithCoolNumber(new BigDecimal('6969'), '69'))
+      .toBe(true);
+    expect(endsWithCoolNumber(new BigDecimal('68.9'), '69'))
+      .toBe(false);
+  });
+
+  it('minMaxCoolNumber(69,4)', () => {
+    const {min, max} = minMaxCoolNumber(new BigDecimal('40'), 69, 4);
+
+    expect(min.getValue()).toBe('39.9969');
+    expect(max.getValue()).toBe('40.0069');
+  });
+
+  it('minMaxCoolNumber(40.2,2)', () => {
+    const {min, max} = minMaxCoolNumber(new BigDecimal('40.2'), 69, 2);
+
+    expect(min.getValue()).toBe('39.69');
+    expect(max.getValue()).toBe('40.69');
+  });
+
+  it('minMaxCoolNumber(69, 0)', () => {
+    const {min, max} = minMaxCoolNumber(new BigDecimal('69'), 69, 0);
+
+    expect(min.getValue()).toBe('69');
+    expect(max.getValue()).toBe('69');
+  });
+
+  it('minMaxCoolNumber(68, 1)', () => {
+    const {min, max} = minMaxCoolNumber(new BigDecimal('68'), 69, 1);
+
+    expect(min.getValue()).toBe('66.9');
+    expect(max.getValue()).toBe('76.9');
+  });
+
+  it('filterValidRange::bounds', () => {
+    const result = filterValidRange({
+      max: '1',
+      min: '0',
+      range: [{max: new BigDecimal('1'), min: new BigDecimal('0')}],
+    });
+
+    expect(result[0].getValue()).toEqual(new BigDecimal('1').getValue());
+    expect(result[1].getValue()).toEqual(new BigDecimal('0').getValue());
+  });
+
+  it('filterValidRange::out-of-bounds', () => {
+    const result = filterValidRange({
+      max: '1',
+      min: '0',
+      range: [{max: new BigDecimal('2'), min: new BigDecimal('-1')}],
+    });
+
+    expect(result.length).toBe(0);
+  });
+
+  it('filterValidRange::partial-min', () => {
+    const result = filterValidRange({
+      max: '1',
+      min: '0',
+      range: [{max: new BigDecimal('2'), min: new BigDecimal('0')}],
+    });
+
+    expect(result.length).toBe(1);
+    expect(result[0].getValue()).toBe('0');
+  });
+
+  it('filterValidRange::partial-max', () => {
+    const result = filterValidRange({
+      max: '1',
+      min: '0',
+      range: [{max: new BigDecimal('1'), min: new BigDecimal('-1')}],
+    });
+
+    expect(result.length).toBe(1);
+    expect(result[0].getValue()).toBe('1');
+  });
+
+  it('abs', () => {
+    expect(abs(new BigDecimal('0')).getValue()).toBe('0');
+    expect(abs(new BigDecimal('1.12324')).getValue()).toBe('1.12324');
+    expect(abs(new BigDecimal('-1.12324')).getValue()).toBe('1.12324');
+  })
+
+  it('pickClosestCoolNumber', () => {
+    expect(
+      pickClosestCoolNumber({
+        value: new BigDecimal('0'),
+        range: [new BigDecimal('-0.5'), new BigDecimal('2')],
+      }).getValue()
+    ).toBe('-0.5');
+    expect(
+      pickClosestCoolNumber({
+        value: new BigDecimal('1.9'),
+        range: [new BigDecimal('-0.5'), new BigDecimal('2')],
+      }).getValue()
+    ).toBe('2');
+  });
+
+  it('toCoolNumber', () => {
+    expect(toCoolNumber({value: '1'}))
+      .toBe('0.9969');
+    expect(toCoolNumber({value: '1', min: '1'}))
+      .toBe('1.0069');
+    expect(toCoolNumber({value: '1', min: '1', max: '1'}))
+      .toBe('1.0000');
+    expect(toCoolNumber({value: '1', min: '1', max: '1', precision: 0}))
+      .toBe('1');
+    expect(toCoolNumber({value: '1', precision: 0}))
+      .toBe('69');
+    expect(toCoolNumber({value: '1', max: '69', precision: 0}))
+      .toBe('69');
+    expect(toCoolNumber({value: '1', max: '68', precision: 0}))
+      .toBe('-69');
+    expect(toCoolNumber({value: '420'}))
+      .toBe('420.0000');
+    expect(toCoolNumber({value: '69'}))
+      .toBe('69.0000');
+  });
+});
+
---
+++ src/index.ts
@@ -0,0 +1,177 @@
+import BigDecimal from 'js-big-decimal';
+
+export type BigDecimalish = ConstructorParameters<typeof BigDecimal>[0];
+
+export type CoolNumberMinMax = {
+  readonly min: BigDecimal;
+  readonly max: BigDecimal;
+};
+
+// Prioritized in coolness.
+export const DEFAULT_COOL_NUMBERS: readonly number[] = [
+  420,
+  69,
+];
+
+export const BD_ONE = new BigDecimal('1');
+export const BD_ZERO = new BigDecimal('0');
+
+export const DEFAULT_PRECISION = 18;
+
+const getValidPrecisionOrThrow = (maybePrecision: number): number => {
+  if (maybePrecision < 0) throw new Error('Precision must be greater than or equal to zero.');
+  return Math.ceil(maybePrecision);
+};
+
+export const toDecimalsString = (e: BigDecimal, maybePrecision = DEFAULT_PRECISION) => {
+
+  const precision = getValidPrecisionOrThrow(maybePrecision);
+  const str = e.getValue();
+  const i = str.indexOf('.');
+
+  const dp = `${precision > 0 ? '.' : ''}`;
+
+  if (i < 0)
+    return `${str}${dp}${[...Array(precision)].map(() => '0').join('')}`;
+
+  const suffix = str.substring(i + 1).substring(0, precision);
+  const padding = Math.max(precision - suffix.length, 0);
+
+  return `${str.substring(0, i)}${dp}${suffix}${[...Array(padding)].map(() => '0').join('')}`;
+};
+
+export const getMinDelta = (maybePrecision = DEFAULT_PRECISION): BigDecimal => {
+  const precision = getValidPrecisionOrThrow(maybePrecision);
+
+  if (maybePrecision === 0)
+    return BD_ONE;
+
+  return new BigDecimal(`0.${[...Array(Math.max(precision - 1, 0))].map(() => '0').join('')}1`);
+};
+
+export const getCoolNumberOrThrow = (coolNumber: number | string): number => {
+  const maybeCoolNumber = typeof coolNumber === 'string'
+      ? parseInt(coolNumber)
+      : coolNumber;
+
+  // Ensure the coolNumber is indeed a string integer (floats are not permitted).
+  if (
+      isNaN(maybeCoolNumber)
+      || maybeCoolNumber < 0
+      || (typeof coolNumber === 'string' && String(maybeCoolNumber) !== coolNumber)
+  )
+    throw new Error(`Expected positive integer coolNumber, encountered "${String(
+        coolNumber
+    )}".`);
+
+  return maybeCoolNumber;
+};
+
+export const endsWithCoolNumber = (e: BigDecimal, maybeCoolNumber: number | string) => {
+  const coolNumber = getCoolNumberOrThrow(maybeCoolNumber);
+  return e.getValue().replace('.', '').endsWith(String(coolNumber));
+};
+
+export const minMaxCoolNumber = (
+  e: BigDecimal,
+  maybeCoolNumber: number | string,
+  precision: number = DEFAULT_PRECISION
+): CoolNumberMinMax => {
+  const coolNumber = getCoolNumberOrThrow(maybeCoolNumber);
+  const minDelta = getMinDelta(precision);
+
+  let max: BigDecimal = e;
+  let min: BigDecimal = e;
+
+  while (!endsWithCoolNumber(max, coolNumber))
+    max = max.add(minDelta);
+
+  while (!endsWithCoolNumber(min, coolNumber))
+    min = min.subtract(minDelta);
+
+  return {min, max};
+};
+
+export const filterValidRange = ({
+  max: maybeMax,
+  min: maybeMin,
+  range,
+}: {
+  readonly max?: BigDecimalish;
+  readonly min?: BigDecimalish;
+  readonly range: readonly CoolNumberMinMax[];
+}) => {
+
+  const max = range
+    .map(({max}) => max)
+    .filter((m) => {
+      if (!maybeMax) return true;
+
+      const r = BigDecimal.compareTo(new BigDecimal(maybeMax).getValue(), m.getValue());
+      return r === 0 || r === 1;
+    });
+
+  const min = range
+    .map(({min}) => min)
+    .filter((m) => {
+      if (!maybeMin) return true;
+
+      const r = BigDecimal.compareTo(new BigDecimal(maybeMin).getValue(), m.getValue());
+      return r === 0 || r === -1;
+    });
+
+  return [...max, ...min];
+};
+
+export const abs = (e: BigDecimal) => BigDecimal.compareTo(e.getValue(), BD_ZERO.getValue()) === -1
+  ? e.negate()
+  : e;
+
+export const pickClosestCoolNumber = ({
+  value,
+  range,
+}: {
+  readonly value: BigDecimal;
+  readonly range: readonly BigDecimal[];
+}) => {
+
+  if (!range.length) return value;
+
+  const deltas = range.map((e) => abs(abs(value).subtract(abs(e))));
+
+  // Find the range with the smallest difference.
+  const sorted = [...deltas].sort(
+    (a, b) => BigDecimal.compareTo(a.getValue(), b.getValue())
+  );
+
+  return range[deltas.indexOf(sorted[0])];
+};
+
+export const toCoolNumber = ({
+  value: defaultValue,
+  max,
+  min,
+  precision = 4,
+  coolNumbers = DEFAULT_COOL_NUMBERS,
+}: {
+  readonly value: BigDecimalish;
+  readonly max?: BigDecimalish;
+  readonly min?: BigDecimalish;
+  readonly precision?: number;
+  readonly coolNumbers?: readonly (string | number)[];
+}) => {
+  const value = new BigDecimal(defaultValue);
+
+  const range = filterValidRange({
+    min,
+    max,
+    range: coolNumbers.map(
+      (maybeCoolNumber: string | number) => minMaxCoolNumber(value, maybeCoolNumber, precision)
+    ),
+  });
+
+  return toDecimalsString(
+    pickClosestCoolNumber({value, range }),
+    precision
+  );
+};
